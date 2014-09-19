8fc03729661cd55714ba2e9fb4ae869ff0f0e9fe
Mac <mackenzie@contrastit.net>
2014-09-18 20:33:43 -0700

    Created directories and files
    Created directories and files

---
+++ Conditionals_Industry/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by MAC on 9/18/14.
+ */
+/*
+Mackenzie Lucas
+September 18, 2014
+Conditionals Assignment
+ */
+
+alert("Testing?");
---
+++ Conditionals_Personal/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by MAC on 9/18/14.
+ */
+/*
+ Mackenzie Lucas
+ September 18, 2014
+ Conditionals Assignment
+ */
+alert("Testing?");
