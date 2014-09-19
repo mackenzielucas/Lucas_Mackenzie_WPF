27041b2ae27b5531710988b6c94fbccef59aea0c
Mac <mackenzie@contrastit.net>
2014-09-18 18:57:15 -0700

    Counted # of pears using if statements
    Counted # of pears using if statements

--- WPF/GoToTrainingWeek3/js/week3.js
+++ WPF/GoToTrainingWeek3/js/week3.js
@@ -28,3 +28,42 @@ console.log(bowlofFruit.length);
 //How can I get the text of "banana"
 console.log(bowlofFruit[1]);
 
+//How do we count the number of pears inside of the whole array
+
+//Create a variable to hold the # of pears
+var totalNumPears = 0;
+
+//Test each fruit and see if it is a pear
+//If it is, lets add it to a total number of pears
+//If Statement - Conditional
+
+if(bowlofFruit[0]==="pear" ){
+    //this code will run if the condition is true
+    //then add 1 to our total number of pears
+    totalNumPears++;
+
+}
+
+//Test all of the fruits in the bowl
+
+if(bowlofFruit[1]==="pear"){
+    totalNumPears++;
+
+}
+
+if(bowlofFruit[2]==="pear"){
+    totalNumPears++;
+
+}
+
+if(bowlofFruit[3]==="pear"){
+    totalNumPears++;
+
+}
+
+if(bowlofFruit[4]==="pear"){
+    totalNumPears++;
+
+}
+
+console.log(totalNumPears+" pears in our bowl");
