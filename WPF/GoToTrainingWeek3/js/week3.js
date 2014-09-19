71d617cea8f51158ead810cab89a1de16bc8a90b
Mac <mackenzie@contrastit.net>
2014-09-18 18:45:24 -0700

    Setup Bowl of Fruit Array
    Covered Basics of Array

--- WPF/GoToTrainingWeek3/js/week3.js
+++ WPF/GoToTrainingWeek3/js/week3.js
@@ -8,4 +8,23 @@ Go To Training Week #3
 Section 01
  */
 
-alert("Testing 1, 2, 3!");
+//alert("Testing 1, 2, 3!");
+
+// Code a check to see how many pears are in a fruit bowl
+
+// Create our "fruit bowl"
+//Array - hold multiple variables under one name - variable of variables
+
+var bowlofFruit = ["apple", "banana", "pear", "peach", "pear"];
+
+console.log(bowlofFruit);
+
+//How long is this array?
+//Array Length
+//Dot Syntax - "use a period"
+
+console.log(bowlofFruit.length);
+
+//How can I get the text of "banana"
+console.log(bowlofFruit[1]);
+
