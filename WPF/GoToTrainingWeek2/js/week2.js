381dbe5d3a872290da6e82bcba86f95ba60eeaf1
Mac <mackenzie@contrastit.net>
2014-09-18 18:35:54 -0700

    Setup Week 3 Files
    Created HTML and js files in web storm

---
+++ WPF/GoToTrainingWeek2/js/week2.js
@@ -0,0 +1,10 @@
+/*
+Mackenzie Lucas
+Sept 10 2014
+WPF Section 02
+Go To Training WEek #2
+ */
+
+//alert("Testing 1, 2, 3");
+
+//Create a calculator for age
---
+++ WPF/GoToTrainingWeek3/js/week3.js
@@ -0,0 +1,11 @@
+/**
+ * Created by MAC on 9/18/14.
+ */
+/*
+Mackenzie Lucas
+Go To Training Week #3
+9-17-2014
+Section 01
+ */
+
+alert("Testing 1, 2, 3!");
